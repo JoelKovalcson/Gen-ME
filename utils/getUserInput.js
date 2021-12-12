46c926d4f6873d8ec25bd9504896eed29af673fc
Joel Kovalcson <joelkovalcson@gmail.com>
2021-12-12 11:33:03 -0500

    Added first couple questions

--- utils/getUserInput.js
+++ utils/getUserInput.js
@@ -1,9 +1,29 @@
+const inquirer = require("inquirer");
+
+const questions = [{
+        type: 'input',
+        name: 'title',
+        message: 'What is your Project Title? (Required)',
+        validate: titleInput => {
+            if (titleInput) return true;
+            else return 'Please enter your Project Title!';
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a project description. (Required)',
+        validate: descInput => {
+            if (descInput) return true;
+            else return 'Please provide a Project Description!';
+        }
+    },
+    
+];
+
 function getUserInput() {
     // Object to hold user input
-    let data = {};
-    
-
-    return data;
+    return inquirer.prompt(questions);
 }
 
 module.exports = getUserInput;
