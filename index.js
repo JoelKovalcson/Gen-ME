7efc76347f98251199f6deb6695329cac165386c
Joel Kovalcson <joelkovalcson@gmail.com>
2021-12-12 11:32:31 -0500

    Changed to reflect promise of user input

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const getUserInput = require('./utils/getUserInput');
 const fs = require('fs');
 
-const questions = [
-
-];
-
 const outputPath = "./out/";
 
 function writeToFile(fileName, data) {
     let md = generateMarkdown(data);
     fs.writeFile(fileName, md, err => {
-        if(err) {
+        if (err) {
             console.log("There was an error generating your README file.");
-        }
-        else {
+        } else {
             console.log("\nREADME successfully generated.\nCheck the 'out' folder for your new README and make sure to save it before making a new one!");
         }
     });
@@ -22,10 +17,11 @@ function writeToFile(fileName, data) {
 
 
 function init() {
-    let data = getUserInput(questions);
-
-    // Hardcode README.md name for now
-    writeToFile(outputPath + 'README.md', data);
+    getUserInput().then(data => {
+        console.log(data);
+        // Hardcode README.md name for now
+        writeToFile(outputPath + 'README.md', data);
+    });
 }
 
 init();
